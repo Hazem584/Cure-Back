697823e9e42b517146656180d19d38be91725ff0
Ayad Emad <ayad.emad144@gmail.com>
2025-11-11 19:39:11 +0200

    Add doctor and appointment management routes and controllers

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const doctorController = require("../controllers/doctorController");
+const {
+  editDoctor,
+  deleteDoctor,
+} = require("../controllers/dashboardController");
 
 // GET /api/v1/doctors/:id/reviews
 router.get("/:id/reviews", doctorController.getDoctorReviews);
@@ -10,5 +14,7 @@ router.get("/:id", doctorController.getDoctorById);
 
 // POST /api/v1/doctors
 router.post("/", doctorController.createDoctor);
+
+router.post("/:id", editDoctor).delete("/:id", deleteDoctor);
 
 module.exports = router;
