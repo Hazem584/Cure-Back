4c2a9dcdd650bce48fd4caf4287c3c04730d9455
Marawan <miroelbehery178@gmail.com>
2025-11-12 02:31:28 +0200

    routes get_one_user,delete_user,update_user completed

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,110 @@
-const Test = require("../models/test_model");
+const mongoose = require("mongoose");
+const User = require("../models/users_test");
 
-const get_one_user = (req, res) => {
-    const user_id = req.params.id
-  res.status(200).json(`get_one_user=${user_id}`);
-};
+const get_one_user = async (req, res) => {
+  const user_id = req.params.id;
 
-module.exports = { get_one_user };
+    //first see if valid id
+     if (!mongoose.Types.ObjectId.isValid(user_id)) {
+       return res.status(400).json({ 
+        code:400,
+        message: "Invalid user ID format" 
+    });
+     }
+
+  try {
+    const found_user = await User.findById(user_id);
+
+    if (found_user) {
+      res.status(200).json({
+        code: 200,
+        data: found_user,
+      });
+    } else {
+      res.status(404).json({
+        code: 404,
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+}
+
+const update_user = async (req,res) =>{
+     const user_id = req.params.id;
+    //first see if valid id
+     if (!mongoose.Types.ObjectId.isValid(user_id)) {
+       return res.status(400).json({ 
+        code:400,
+        message: "Invalid user ID format" 
+    });
+     }
+
+     try {
+        //check if data correct then update
+       const updatedUser = await User.findByIdAndUpdate(user_id, req.body, {
+         new: true,
+         runValidators: true,
+       });
+
+       if (!updatedUser) {
+         return res.status(404).json({ message: "User not found" });
+       }
+
+       res.status(200).json({
+         code: 200,
+         message: "User updated successfully",
+         data: updatedUser,
+       });
+     } catch (err) {
+       res.status(500).json({
+         code: 500,
+         message: "Error updating user",
+         error: err.message,
+       });
+     }
+}
+
+const delete_user = async (req, res)=>{
+  const user_id = req.params.id;
+
+  //first see if valid id
+  if (!mongoose.Types.ObjectId.isValid(user_id)) {
+    return res.status(400).json({
+      code: 400,
+      message: "Invalid user ID format",
+    });
+  }
+  try{
+
+    const deleted_user = await User.findByIdAndDelete(user_id);
+    console.log(deleted_user);
+    if(!deleted_user){
+        res.status(404).json({
+            code:404,
+            message:"User_not_exist"
+        })
+    }
+    else{
+        res.status(200).json({
+            code:200,
+            message:"User deleted Success",
+            data:deleted_user,
+    })
+    }
+
+
+  }catch(err){
+    res.status(500).json({
+        code:500,
+        message:"Error deleting user",
+        error: err.message
+    })
+  }
+}
+module.exports = { get_one_user, update_user, delete_user }
