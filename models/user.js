49de0a8e4ee8279ad145f4b4b41c69d80fb3f2fb
Ayad Emad <ayad.emad144@gmail.com>
2025-11-13 16:00:44 +0200

    Refactor user schema for improved readability and consistency

--- models/user.js
+++ models/user.js
@@ -1,36 +1,39 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+
+    avatarUrl: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "admin",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String,
-  },
-  
-  avatarUrl: {
-    type: String,
-    default: ""
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"], 
-    default: "user"      
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model("Users", userSchema);
 
