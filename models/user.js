8eec571c63a437616a8514774fa49386009865c4
Hazem Mohammed <hazemhkcyy@gmail.com>
2025-11-13 04:46:23 +0200

    test

--- models/user.js
+++ models/user.js
@@ -32,6 +32,6 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-const User = mongoose.model("Users", userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
