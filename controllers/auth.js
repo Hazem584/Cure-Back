53869cc40fd69ebcd69380ebdff7cd0eefca5a60
miroElbehery <miroelbehery178@gmail.com>
2025-11-29 17:23:28 +0200

    added the info be sent when when logged in avatarurl and address

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,6 +93,10 @@ const login = async (req, res) => {
           id: user._id,
           name: user.name,
           email: user.email,
+          // added address and avatar url
+          address:user.address,
+          avataUrl:user.avatarUrl,
+          // -------
           role: user.role,
         },
         token,
